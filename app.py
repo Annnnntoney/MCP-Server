09bf83d13943eb54c2c2def72f52d20687a33010
Antoney <antoney0405@gmail.com>
2026-07-21 02:31:59 +0800

    Bulid Pie Chart Tool

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 # pip install fastmcp
+
+import io
 from fastmcp import FastMCP
+from fastmcp.utilities.types import Image
+import matplotlib.pyplot as plt
 
 app=FastMCP("My MCP Server")
 @app.tool
@@ -8,3 +12,11 @@ def add(n1:int, n2:int)->int:
     return n1+n2
 
 # fastmcp run app.py:app --transport http --port 8000
+
+@app.tool
+def draw_pie_chart(numbers:list[int])->Image:
+    """"Draw Pie Chart"""
+    plt.pie(numbers)
+    buffer=io.BytesIO()
+    plt.savefig(buffer, format="png")
+    return Image(data=buffer, format="png")
